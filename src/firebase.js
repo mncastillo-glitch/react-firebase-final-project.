6b11e0169669270fd8d6824c8888eed2a7566bdb
mematello <marcuskentoliver1205@gmail.com>
2026-04-07 10:02:58 +0800

    Preparing for Vercel deployment

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,11 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore"; // Add this line!
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDOYodyCx1R5ekUONd41X7A4N9oX9xqUK8",
+  apiKey: "AIzaSyDOYodyCx1R5ekUOND41X7A4N9oX9xqUK8",
   authDomain: "note-app-f2981.firebaseapp.com",
   projectId: "note-app-f2981",
-  storageBucket: "note-app-f2981.firebasestorage.app",
+  storageBucket: "note-app-f2981.appspot.com",
   messagingSenderId: "836066568064",
   appId: "1:836066568064:web:abe39eacd935d8a153c365",
   measurementId: "G-0Z44BMDYPT"
@@ -18,4 +13,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+// Export the database so App.js can use it
+export const db = getFirestore(app);
